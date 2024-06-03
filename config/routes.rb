d0ccd44d900609cafa3bfc047456f0e4d6f35bb7
EC2 Default User <ec2-user@ip-172-31-34-9.ap-northeast-1.compute.internal>
2024-06-03 13:12:07 +0000

    homes:top,about作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  #管理者用(URL/admin/sign_in)
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  #会員用(URL/users/sign_in)
+  devise_for :users, skip:[:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  root to: "public/homes#top"
+
+  scope module: 'public' do
+    get 'about', to: 'homes#about', as: :homes_about
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
