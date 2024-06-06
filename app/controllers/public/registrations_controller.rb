76a7cc79655229ad76609e928c31aedbbe58a5e5
EC2 Default User <ec2-user@ip-172-31-34-9.ap-northeast-1.compute.internal>
2024-06-06 06:03:50 +0000

    新規登録(レイアウト未)

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  #before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_permitted_parameters, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -41,14 +42,18 @@ class Public::RegistrationsController < Devise::RegistrationsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :account_id, :phone_number])
+  #def configure_sign_up_params
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :account_id, :phone_number])
+  #end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :account_id, :introduction, :phone_number])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :account_id, :phone_number])
-  end
+  #def configure_account_update_params
+    #devise_parameter_sanitizer.permit(:account_update, keys: [:name, :account_id, :phone_number])
+  #end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
@@ -56,7 +61,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   end
 
   # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    posts_path
-  end
+  #def after_inactive_sign_up_path_for(resource)
+    #posts_path
+  #end
 end
