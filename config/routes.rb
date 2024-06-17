64477a90968566b92f5bafed94328f29ce10173a
EC2 Default User <ec2-user@ip-172-31-34-9.ap-northeast-1.compute.internal>
2024-06-17 07:32:25 +0000

    投稿履歴一覧途中

--- config/routes.rb
+++ config/routes.rb
@@ -22,10 +22,15 @@ Rails.application.routes.draw do
         delete :hide, as: 'users_hide'
       end
     end
-    
+
     resources :posts, only: [:index, :show, :edit, :create, :update, :destroy] do
       resources :comments, only: [:create, :destroy]
     end
+  end
+
+  namespace :admin do
+    root to: 'homes#top'
+
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
