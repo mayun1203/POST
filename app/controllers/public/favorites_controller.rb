ea14b70eb6d575d633e6883e8483b3fbb491a93e
EC2 Default User <ec2-user@ip-172-31-34-9.ap-northeast-1.compute.internal>
2024-06-23 15:01:12 +0000

    通知機能実装

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,14 +1,17 @@
 class Public::FavoritesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    @favorite = current_user.favorites.new(post_id: post.id)
-    @favorite.save
-    render 'replace_btn'
 
-    #投稿にいいねされた時の
-    if current_user != @post.user
-      @post.create_notification_favorite_post!(current_user)
+    #投稿にいいねされた時の通知
+
+
+    @post = Post.find(params[:post_id])
+    @favorite = current_user.favorites.new(post_id: @post.id)
+    if @favorite.save
+      if current_user != @post.user
+        @post.create_notification_favorite_post!(current_user)
+      end
     end
+    render 'replace_btn'
   end
 
   def destroy
--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -8,15 +8,14 @@ class Public::NotificationsController < ApplicationController
 
     #確認済みの通知を取得
     @checked_notifications = @notices.where(is_checked: true).limit(20)
+    render :index, layout: false
 
-    #通知を確認済みに更新
-    current_user.passive_notifications.update_all(is_checked: true)
-    render partial: "index"
   end
 
   #通知を一括して確認済みにに
   def update_checked
-    current_user.passive_notifications.update_all(is_checked: true)
+    #byebug
+    #current_user.passive_notifications.update_all(is_checked: true)
     head :no_content
   end
 end
