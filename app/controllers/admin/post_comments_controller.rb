9c6d78cc5239167f838a23c21e740b24de1b1448
EC2 Default User <ec2-user@ip-172-31-34-9.ap-northeast-1.compute.internal>
2024-08-01 12:24:39 +0000

    管理者:コメント一覧・削除追加

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,12 +1,12 @@
 class Admin::PostCommentsController < ApplicationController
 
   def index
-    @post_comments = PostComment.all
+    @post_comments = Comment.order(create_at: :desc).page(params[:page]).per(10)
     @users = User.all
   end
 
   def destroy
-    @post_comment = PostComment.find(params[:id])
+    @post_comment = Comment.find(params[:id])
     @post_comment.destroy
     redirect_to admin_post_comments_path
   end
